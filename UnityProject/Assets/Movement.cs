0259f71fd3f5535f58f4fd90c5fc3ffa391b6ac1
Ben Waters <bjw69798@uga.edu>
2019-04-22 23:40:32 -0400

    Lots of Script Stuff
    Co-Authored-By: kiannapicard <kiannapicard@users.noreply.github.com>

---
+++ UnityProject/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1.2f;
+
+    public float gravity = 9.6f;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector3 velocity = direction * speed;
+        velocity = Camera.main.transform.TransformDirection(velocity);
+        velocity.y -= gravity;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
