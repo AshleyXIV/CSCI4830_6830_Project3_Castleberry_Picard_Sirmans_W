fd43f87535b2d2a840c3a0890e18d8ceae5c449b
Ben Waters <bjw69798@uga.edu>
2019-04-28 21:25:05 -0400

    Script for Advertisment
    Co-Authored-By: kiannapicard <kiannapicard@users.noreply.github.com>
    Co-Authored-By: Matt Castleberry <mootcastle@users.noreply.github.com>

--- UnityProject/Assets/Scripts/ModelSwap.cs
+++ UnityProject/Assets/Scripts/ModelSwap.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ModelSwap : MonoBehaviour
 {
+
+    
+    public GameObject targetFood;
+    GameObject waste = GameObject.FindGameObjectWithTag("Pop_Drop_Loc_1");
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+
+        if (Input.GetKeyDown("s"))
+        {
+
+            targetFood.transform.position = transform.position;
+            transform.position = waste.transform.position;
+        }
     }
 }
