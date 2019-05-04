354b960c4433bdfe20cc6700e0c6d73bf35caaf5
Ben Waters <bjw69798@uga.edu>
2019-05-04 01:07:28 -0400

    Notes + Model Update
    Omlette refining
    
    Co-Authored-By: kiannapicard <kiannapicard@users.noreply.github.com>

---
+++ UnityProject/Assets/Scripts/Egg_Detection.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Egg_Detection : MonoBehaviour
+{
+
+    public GameObject egg1;
+    public GameObject egg2;
+    public GameObject egg3;
+
+    
+    GameObject waste;
+    Collider col;
+    
+
+    bool cracked1 = false;
+    bool cracked2 = false;
+    bool cracked3 = false;
+    bool stacked1 = false;
+    bool stacked2 = false;
+    bool stacked3 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject waste = GameObject.FindGameObjectWithTag("Pop_Drop_Loc_1");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stacked1 && cracked1)
+        {
+            egg1.transform.position = waste.transform.position;
+        }
+        if (stacked2 && cracked2)
+        {
+            Debug.Log("Egg 2 has been yeeted");
+            egg2.transform.position = waste.transform.position;
+        }
+        if (stacked3 && cracked3)
+        {
+            egg3.transform.position = waste.transform.position;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "eggs_1")
+        {
+            Destroy(egg2);
+        }
+    }
+}
