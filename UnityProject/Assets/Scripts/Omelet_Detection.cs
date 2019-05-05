84810986a2bfff45dc3e4bb56e980a5706876239
Ben Waters <bjw69798@uga.edu>
2019-05-05 00:06:20 -0400

    Level Changes
    Co-Authored-By: kiannapicard <kiannapicard@users.noreply.github.com>

---
+++ UnityProject/Assets/Scripts/Omelet_Detection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Omelet_Detection : MonoBehaviour
+{
+
+    public GameObject seasonedScram;
+    public GameObject megaBowl;
+    public GameObject uncookedOmelet;
+
+    bool pouredMixture = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pouredMixture)
+        {
+            uncookedOmelet.SetActive(true);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "megaBowl")
+        {
+            Destroy(seasonedScram);
+            pouredMixture = true;
+        }
+    }
+    }
